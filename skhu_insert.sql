b5a375048288aed94973c5cbd586e4d12c992f30
skhu202112021-creator <skhu202112021@gmail.com>
2026-04-20 10:56:05 +0900

    Update skhu_insert.sql
    오류 수정

--- skhu_insert.sql
+++ skhu_insert.sql
@@ -3,8 +3,8 @@ insert into students (name, department, login_id, login_pw)
 values ('학생1', '소프트웨어학과', 'student1', 'password1');
 insert into professors (name, department, login_id, login_pw)
 values ('교수1', '소프트웨어학과', 'professor1', 'password1');
-insert into subjects (name, department, professor)
-values ('소프트웨어캡스톤디자인', '소프트웨어학과', 'professor1');
+insert into subjects (name, department, professor, capacity)
+values ('소프트웨어캡스톤디자인', '소프트웨어학과', 'professor1',100);
 insert into departments (name)
 values ('소프트웨어학과');
 select * from students, professors, subjects, departments;
